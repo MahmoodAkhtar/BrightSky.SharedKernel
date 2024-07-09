be3d3c2fbc80f773f88f084ffff0c61e5028455c
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-07-09 13:26:05 +0100

    More Option<TValue> unit tests. Found bugs. Resulted in improved Option<TValue> impl after fixes.

--- src/BrightSky.SharedKernel/Option.cs
+++ src/BrightSky.SharedKernel/Option.cs
@@ -2,15 +2,20 @@
 
 public readonly record struct Option<TValue>
 {
-    private readonly TValue? _value;
+    private readonly TValue _value;
 
-    private Option(TValue value) => _value = value;
+    private Option(TValue value)
+    {
+        IsSome = true;
+        _value = value;
+    }
 
-    public bool IsNone => _value is null;
-    public bool IsSome => !IsNone;
+    public readonly bool IsSome;
+    public bool IsNone => !IsSome;
+    
     public TValue Value => IsSome ? _value! : throw new NullReferenceException();
 
-    public static Option<TValue> None() => new();
+    public static Option<TValue> None => default;
     public static Option<TValue> Some(TValue value) => new(value);
 
     public static implicit operator Option<TValue>(TValue value) => Some(value);
@@ -22,13 +27,13 @@ public static class OptionExtensions
         <TValue, TResult>(
             this Option<TValue> option,
             Func<TValue, TResult> map)
-        => option.IsSome ? Option<TResult>.Some(map(option.Value)) : Option<TResult>.None();
+        => option.IsSome ? Option<TResult>.Some(map(option.Value)) : Option<TResult>.None;
 
     public static Option<TResult> Bind
         <TValue, TResult>(
             this Option<TValue> option,
             Func<TValue, Option<TResult>> bind)
-        => option.IsSome ? bind(option.Value) : Option<TResult>.None();
+        => option.IsSome ? bind(option.Value) : Option<TResult>.None;
 
     public static TValue Reduce
         <TValue>(
--- tests/BrightSky.SharedKernel.Tests/OptionTests.cs
+++ tests/BrightSky.SharedKernel.Tests/OptionTests.cs
@@ -3,7 +3,7 @@
 public class OptionTests
 {
     [Fact]
-    public void When_OptionSome_ThenAssert_IsSome_EqualsTrue()
+    public void When_OptionSome_WithString_ThenAssert_IsSome_EqualsTrue()
     {
         var option = Option<string>.Some("some string");
         
@@ -11,7 +11,7 @@ public class OptionTests
     }
     
     [Fact]
-    public void When_OptionSome_ThenAssert_IsNone_EqualsFalse()
+    public void When_OptionSome_WithString_ThenAssert_IsNone_EqualsFalse()
     {
         var option = Option<string>.Some("some string");
         
@@ -19,7 +19,7 @@ public class OptionTests
     }
     
     [Fact]
-    public void When_OptionSome_ThenAssert_Value_EqualsExpected()
+    public void When_OptionSome_WithString_ThenAssert_Value_EqualsExpected()
     {
         var expected = "some string";
         var option = Option<string>.Some(expected);
@@ -28,37 +28,98 @@ public class OptionTests
     }
     
     [Fact]
-    public void When_OptionNone_ThenAssert_IsSome_EqualsFalse()
+    public void When_OptionNone_WithString_ThenAssert_IsSome_EqualsFalse()
     {
-        var option = Option<string>.None();
+        var option = Option<string>.None;
         
         Assert.False(option.IsSome);
     }
     
     [Fact]
-    public void When_OptionNone_ThenAssert_IsNone_EqualsTrue()
+    public void When_OptionNone_WithString_ThenAssert_IsNone_EqualsTrue()
     {
-        var option = Option<string>.None();
+        var option = Option<string>.None;
         
         Assert.True(option.IsNone);
     }
     
     [Fact]
-    public void When_OptionNone_ThenAssert_Value_ThrowsNullReferenceException()
+    public void When_OptionNone_WithString_ThenAssert_Value_ThrowsNullReferenceException()
     {
-        var option = Option<string>.None();
-        var func = () => option.Value;
+        var option = Option<string>.None;
         
-        Assert.Throws<NullReferenceException>(func);
+        Assert.Throws<NullReferenceException>(() => option.Value);
     }
     
     [Fact]
-    public void When_Option_ThenAssert_ImplicitOperator_EqualsExpected()
+    public void When_Option_WithString_ThenAssert_ImplicitOperator_EqualsExpected()
     {
         var value = "some string";
         var option = (Option<string>)value;
         
         Assert.IsType<Option<string>>(option);
+        Assert.Equal(value, option.Value);
+        Assert.True(option.IsSome);
+        Assert.False(option.IsNone);
+    }
+    
+    [Fact]
+    public void When_OptionSome_WithInt_ThenAssert_IsSome_EqualsTrue()
+    {
+        var option = Option<int>.Some(123);
+        
+        Assert.True(option.IsSome);
+    }
+    
+    [Fact]
+    public void When_OptionSome_WithInt_ThenAssert_IsNone_EqualsFalse()
+    {
+        var option = Option<int>.Some(123);
+        
+        Assert.False(option.IsNone);
+    }
+    
+    [Fact]
+    public void When_OptionSome_WithInt_ThenAssert_Value_EqualsExpected()
+    {
+        var expected = 123;
+        var option = Option<int>.Some(expected);
+        
+        Assert.Equal(expected, option.Value);
+    }
+    
+    
+    [Fact]
+    public void When_OptionNone_WithInt_ThenAssert_IsSome_EqualsFalse()
+    {
+        var option = Option<int>.None;
+        
+        Assert.False(option.IsSome);
+    }
+    
+    [Fact]
+    public void When_OptionNone_WithInt_ThenAssert_IsNone_EqualsTrue()
+    {
+        var option = Option<int>.None;
+        
+        Assert.True(option.IsNone);
+    }
+    
+    [Fact]
+    public void When_OptionNone_WithInt_ThenAssert_Value_ThrowsNullReferenceException()
+    {
+        var option = Option<int>.None;
+        
+        Assert.Throws<NullReferenceException>(() => option.Value);
+    }
+    
+    [Fact]
+    public void When_Option_WithInt_ThenAssert_ImplicitOperator_EqualsExpected()
+    {
+        var value = 123;
+        var option = (Option<int>)value;
+        
+        Assert.IsType<Option<int>>(option);
         Assert.Equal(value, option.Value);
         Assert.True(option.IsSome);
         Assert.False(option.IsNone);
