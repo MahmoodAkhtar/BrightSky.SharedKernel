9a56c58690877cbe7c5a5f47d47194c6d1629fd7
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-07-09 08:32:22 +0100

    Adding Option<TValue> unit tests

---
+++ tests/BrightSky.SharedKernel.Tests/OptionTests.cs
@@ -0,0 +1,66 @@
+﻿namespace BrightSky.SharedKernel.Tests;
+
+public class OptionTests
+{
+    [Fact]
+    public void When_OptionSome_ThenAssert_IsSome_EqualsTrue()
+    {
+        var option = Option<string>.Some("some string");
+        
+        Assert.True(option.IsSome);
+    }
+    
+    [Fact]
+    public void When_OptionSome_ThenAssert_IsNone_EqualsFalse()
+    {
+        var option = Option<string>.Some("some string");
+        
+        Assert.False(option.IsNone);
+    }
+    
+    [Fact]
+    public void When_OptionSome_ThenAssert_Value_EqualsExpected()
+    {
+        var expected = "some string";
+        var option = Option<string>.Some(expected);
+        
+        Assert.Equal(expected, option.Value);
+    }
+    
+    [Fact]
+    public void When_OptionNone_ThenAssert_IsSome_EqualsFalse()
+    {
+        var option = Option<string>.None();
+        
+        Assert.False(option.IsSome);
+    }
+    
+    [Fact]
+    public void When_OptionNone_ThenAssert_IsNone_EqualsTrue()
+    {
+        var option = Option<string>.None();
+        
+        Assert.True(option.IsNone);
+    }
+    
+    [Fact]
+    public void When_OptionNone_ThenAssert_Value_ThrowsNullReferenceException()
+    {
+        var option = Option<string>.None();
+        var func = () => option.Value;
+        
+        Assert.Throws<NullReferenceException>(func);
+    }
+    
+    [Fact]
+    public void When_Option_ThenAssert_ImplicitOperator_EqualsExpected()
+    {
+        var value = "some string";
+        var option = (Option<string>)value;
+        
+        Assert.IsType<Option<string>>(option);
+        Assert.Equal(value, option.Value);
+        Assert.True(option.IsSome);
+        Assert.False(option.IsNone);
+    }
+}
