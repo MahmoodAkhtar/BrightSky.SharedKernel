efd2f567ffcabb3ff4451ecbaa2369c9d0cb7658
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-07-10 08:57:57 +0100

    Adding a way to do a precondition check with a fluent like syntax which returns an Option<Error> along with unit tests for it.

---
+++ src/BrightSky.SharedKernel/Precondition.cs
@@ -0,0 +1,28 @@
+﻿namespace BrightSky.SharedKernel;
+
+public static class Precondition
+{
+    public static Result<TValue, Error> Requires<TValue>(TValue value) => Result<TValue, Error>.Success(value);
+
+    public static Result<TValue, Error> BeTrueFor<TValue>(this Result<TValue, Error> result, Func<TValue, bool> predicate)
+        => result.IsFailure ? result : predicate(result.Value) 
+            ? result 
+            : Result<TValue, Error>.Failure(Error.Failure(
+                "Precondition.BeTrueFor", 
+                "Func<TValue, bool> predicate was false"));
+
+
+    public static Result<TValue, Error> BeFalseFor<TValue>(this Result<TValue, Error> result, Func<TValue, bool> predicate)
+        => result.IsFailure ? result : !predicate(result.Value) 
+            ? result 
+            : Result<TValue, Error>.Failure(Error.Failure(
+                "Precondition.BeFalseFor", 
+                "Func<TValue, bool> predicate was true"));    
+    
+    public static Option<Error> OrError<TValue>(this Result<TValue, Error> result)
+        => result.IsFailure ? Option<Error>.Some(result.Error) : Option<Error>.None;
+    
+    public static Option<Error> OrError<TValue>(this Result<TValue, Error> result, Error error)
+        => result.IsFailure ? Option<Error>.Some(error) : Option<Error>.None;
+
+}
---
+++ tests/BrightSky.SharedKernel.Tests/PreconditionTests.cs
@@ -0,0 +1,106 @@
+﻿namespace BrightSky.SharedKernel.Tests;
+
+public class PreconditionTests
+{
+    [Fact]
+    public void 
+    PreconditionRequires_Value_BeTrueFor_Predicate_OrErrorWithError_PredicateIsFalse_Assert_Error_EqualsExpected()
+    {
+        string? value = null;
+
+        var expected = Error.Failure("MyCode", $"{nameof(value)} {value} is not null");
+        var option = Precondition.Requires(value).BeTrueFor(x => x is not null).OrError(expected);
+        
+        Assert.True(option.IsSome);
+        Assert.Equal(expected, option.Value);
+    }
+    
+    [Fact]
+    public void 
+    PreconditionRequires_Value_BeTrueFor_Predicate_OrErrorWithError_PredicateIsTrue_Assert_NoError()
+    {
+        string? value = "some value";
+
+        var error = Error.Failure("MyCode", $"{nameof(value)} {value} is not null");
+        var option = Precondition.Requires(value).BeTrueFor(x => x is not null).OrError(error);
+        
+        Assert.True(option.IsNone);
+        Assert.Equal(Option<Error>.None, option);
+    }
+    
+    [Fact]
+    public void 
+    PreconditionRequires_Value_BeFalseFor_Predicate_OrErrorWithError_PredicateIsTrue_Assert_Error_EqualsExpected()
+    {
+        string? value = null;
+
+        var expected = Error.Failure("MyCode", $"{nameof(value)} {value} is null");
+        var option = Precondition.Requires(value).BeFalseFor(x => x is null).OrError(expected);
+        
+        Assert.True(option.IsSome);
+        Assert.Equal(expected, option.Value);
+    }
+    
+    [Fact]
+    public void 
+    PreconditionRequires_Value_BeFalseFor_Predicate_OrErrorWithError_PredicateIsFalse_Assert_Error_EqualsExpected()
+    {
+        string? value = "some value";
+
+        var error = Error.Failure("MyCode", $"{nameof(value)} {value} is null");
+        var option = Precondition.Requires(value).BeFalseFor(x => x is null).OrError(error);
+        
+        Assert.True(option.IsNone);
+        Assert.Equal(Option<Error>.None, option);
+    }
+    
+    [Fact]
+    public void 
+    PreconditionRequires_Value_BeTrueFor_Predicate_OrError_PredicateIsFalse_Assert_Error_EqualsExpected()
+    {
+        string? value = null;
+
+        var expected = Error.Failure("Precondition.BeTrueFor", "Func<TValue, bool> predicate was false");
+        var option = Precondition.Requires(value).BeTrueFor(x => x is not null).OrError();
+        
+        Assert.True(option.IsSome);
+        Assert.Equal(expected, option.Value);
+    }
+    
+    [Fact]
+    public void 
+    PreconditionRequires_Value_BeTrueFor_Predicate_OrError_PredicateIsTrue_Assert_NoError()
+    {
+        string? value = "some value";
+
+        var option = Precondition.Requires(value).BeTrueFor(x => x is not null).OrError();
+        
+        Assert.True(option.IsNone);
+        Assert.Equal(Option<Error>.None, option);
+    }
+    
+    [Fact]
+    public void 
+    PreconditionRequires_Value_BeFalseFor_Predicate_OrError_PredicateIsTrue_Assert_Error_EqualsExpected()
+    {
+        string? value = null;
+
+        var expected = Error.Failure("Precondition.BeFalseFor", "Func<TValue, bool> predicate was true");
+        var option = Precondition.Requires(value).BeFalseFor(x => x is null).OrError();
+        
+        Assert.True(option.IsSome);
+        Assert.Equal(expected, option.Value);
+    }
+    
+    [Fact]
+    public void 
+    PreconditionRequires_Value_BeFalseFor_Predicate_OrError_PredicateIsFalse_Assert_Error_EqualsExpected()
+    {
+        string? value = "some value";
+
+        var option = Precondition.Requires(value).BeFalseFor(x => x is null).OrError();
+        
+        Assert.True(option.IsNone);
+        Assert.Equal(Option<Error>.None, option);
+    }
+}
