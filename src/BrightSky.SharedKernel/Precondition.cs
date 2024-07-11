5af1fcf46749b4b587364df447e1d7355832eb5e
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-07-11 13:19:28 +0100

    Formatting fixes

--- src/BrightSky.SharedKernel/Precondition.cs
+++ src/BrightSky.SharedKernel/Precondition.cs
@@ -10,23 +10,23 @@ public static class Precondition
             Specification<TValue> specification)
         => result.Match(
             success => 
-                specification.IsSatisfiedBy(success) 
-                    ? success 
+                specification.IsSatisfiedBy(success)
+                    ? success
                     : Result<TValue, Option<Error>>.Failure(Error.Failure(
-                        $"Precondition.{specification.GetType().Name}",
-                        $"Specification {specification.GetType().Name} was not met")),
-                failure => Option<Error>.Some(failure));
+                    $"Precondition.{specification.GetType().Name}",
+                    $"Specification {specification.GetType().Name} was not met")),
+            failure => Option<Error>.Some(failure));
     
     public static TValue ThenAssignOrThrow<TValue, TException>(this Result<TValue, Option<Error>> result)
         where TException : Exception
         => result
             .Match<TValue, Option<Error>, Result<TValue, Option<TException>>>(
-            success => success,
-            failure => failure
-                .Map(error => (TException)Activator.CreateInstance(typeof(TException),
-                    $"{Error.GetNameFor(error.Type)} {error.Code} {error.Description}")!)
-                .Tap(exception => throw exception))
+                success => success,
+                failure => failure
+                    .Map(error => (TException)Activator.CreateInstance(typeof(TException),
+                        $"{Error.GetNameFor(error.Type)} {error.Code} {error.Description}")!)
+                    .Tap(exception => throw exception))
             .Match(
-            success => success,
-            failure => default!);
+                success => success,
+                failure => default!);
 }
